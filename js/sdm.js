2134635a1d28c12bb330430a315e35fad0aac98c
Jason LaPorte <jason@agoragames.com>
2012-01-28 10:33:28 -0500

    Simplified the interface to the SDM functions.

--- js/sdm.js
+++ js/sdm.js
@@ -1,7 +1,10 @@
 "use strict"
 
-function sdmReset (arr) {
+function sdmReset (arr, bits, range) {
   var i = arr.length
+
+  arr[--i] = range
+  arr[--i] = bits
 
   while (i--)
     arr[i] = 0
@@ -19,33 +22,37 @@ function registerGet (arr, offset, accumulator) {
     accumulator[--i] += arr[--offset]
 }
 
-function sdmPut (arr, key, value, bits, range) {
-  var seed = [
-        0.76758391689509150,
-        0.54703062842600050,
-        0.08701058942824602,
-        1
-      ],
-      i
-
-  for (i = Math.floor (arr.length / bits) * bits; i; i -= bits)
-    if (distance (key, random (seed, bits)) <= range)
-      registerPut (arr, i, value, bits)
-}
-
-function sdmGet (arr, key, bits, range) {
-  var accumulator = new Array (bits),
+function sdmPut (arr, key, value) {
+  var i = arr.length,
+      range = arr[--i],
+      bits = arr[--i],
       seed = [
         0.76758391689509150,
         0.54703062842600050,
         0.08701058942824602,
         1
-      ],
-      i
+      ]
 
-  sdmReset (accumulator)
+  for (i = Math.floor (i / bits) * bits; i; i -= bits)
+    if (distance (key, random (seed, bits)) <= range)
+      registerPut (arr, i, value, bits)
+}
 
-  for (i = Math.floor (arr.length / bits) * bits; i; i -= bits)
+function sdmGet (arr, key) {
+  var i = arr.length,
+      range = arr[--i],
+      bits = arr[--i],
+      accumulator = new Array (bits),
+      seed = [
+        0.76758391689509150,
+        0.54703062842600050,
+        0.08701058942824602,
+        1
+      ]
+
+  sdmReset (accumulator, 0, 0) // HACK
+
+  for (i = Math.floor (i / bits) * bits; i; i -= bits)
     if (distance (key, random (seed, bits)) <= range)
       registerGet (arr, i, accumulator)
 
